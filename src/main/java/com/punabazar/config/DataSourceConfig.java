9f6cf75b3902fbd0805d8e37447207253cca5801
Jatin Shinde <jatinshinde@example.com>
2026-08-19 23:43:31 +0530

    Override external cloud environment variables with primary embedded datasource configuration in DataSourceConfig.java

---
+++ src/main/java/com/punabazar/config/DataSourceConfig.java
@@ -0,0 +1,23 @@
+package com.punabazar.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    @Primary
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:punabazar;DB_CLOSE_ON_EXIT=FALSE;MODE=MySQL");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+}
